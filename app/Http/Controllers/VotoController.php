c58e6438b32f661bb125cd936d99e7cf8abf2f00
Juan Fajardo <bett0@potosi.bo>
2021-02-26 14:54:54 -0400

    @voto

--- app/Http/Controllers/VotoController.php
+++ app/Http/Controllers/VotoController.php
@@ -7,12 +7,15 @@ use Illuminate\Http\Request;
 
 class VotoController extends Controller
 {
-  public function __construct(){
-    $this->middleware('auth');
-  }
 
   public function index(Request $request){
-    $datos = Voto::all();
+    $datos = Voto::Where('tipo', '=', 'ALCALDE')->where('aceptado', '=', '1')
+              ->join('mesas',     'votos.id_mesa',     '=', 'mesas.id')
+              ->join('recintos',  'mesas.id_recinto', '=', 'recintos.id')
+              ->join('zonas',     'mesas.id_zona',    '=', 'zonas.id')
+              ->orderBy('votos.id', 'asc')
+              ->get();
+
     if ($request->ajax()) {
       return $datos;
     }else{
@@ -33,8 +36,25 @@ class VotoController extends Controller
   }
 
   public function update(Request $request, $id){
-    $dato = Voto::find($id);
-    $dato->fill($request->all());
+    //return $request->all();
+    $dato = Voto::find($request->id);
+
+    $dato->as     = $request->as;
+    $dato->cc     = $request->cc;
+    $dato->mas       = $request->mas;
+    $dato->adn       = $request->adn;
+    $dato->jap       = $request->jap;
+    $dato->mcp       = $request->mcp;
+    $dato->ucs       = $request->ucs;
+    $dato->puka       = $request->puka;
+    $dato->mds       = $request->mds;
+    $dato->mts       = $request->mts;
+    $dato->fpv       = $request->fpv;
+    $dato->mop       = $request->mop;
+    $dato->nulo       = $request->nulo;
+    $dato->blanco       = $request->blanco;
+    $dato->total       = $request->total;
+    $dato->observacion       = $request->observacion;
     $dato->save();
     return redirect('/Voto');
   }
@@ -47,6 +67,18 @@ class VotoController extends Controller
     }else{
       return redirect('/Voto');
     }
+  }
+
+  public function votoVer($id){
+    $datos = Voto::Where('votos.id', '=', $id)
+              ->join('mesas',     'votos.id_mesa',     '=', 'mesas.id')
+              ->join('recintos',  'mesas.id_recinto', '=', 'recintos.id')
+              ->join('personas',     'votos.id_persona',    '=', 'personas.id')
+              ->select('votos.*', 'recintos.recinto', 'personas.*', 'mesas.*', 'votos.id as votoId')
+              ->get();
+    return $datos;
+
+
   }
 
 }
