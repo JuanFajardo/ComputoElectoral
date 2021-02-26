ea89f78c12b5539eb68c717b450cef69c48d8bab
Juan Fajardo <bett0@potosi.bo>
2021-02-26 14:57:40 -0400

    @rutas

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::get('recintos', 'RecintoController@getRecintos')->name('recintos.listar');
+Route::get('recintos/{id}', 'RecintoController@getMesa')->name('recintos.mesas');
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Voto/votoVer/{id}', 'VotoController@votoVer');
+Route::resource('/Voto',    'VotoController');
+Route::resource('/Persona', 'PersonaController');
+Route::resource('/Mesa',    'MesaController');
+Route::resource('/Recinto', 'RecintoController');
+Route::resource('/Zona',    'ZonaController');
+Route::resource('/Distrito','DistritoController');
