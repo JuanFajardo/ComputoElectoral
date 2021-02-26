f792f925355361bb3d9c2f0a9915549791e306d7
Juan Fajardo <bett0@potosi.bo>
2021-02-26 14:53:43 -0400

    @persona

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -7,9 +7,6 @@ use Illuminate\Http\Request;
 
 class PersonaController extends Controller
 {
-  public function __construct(){
-    $this->middleware('auth');
-  }
 
   public function index(Request $request){
     $datos = Persona::all();
